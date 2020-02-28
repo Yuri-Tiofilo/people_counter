d6b8dd82337f97d941122a452fe358d22b4e31c5
Yuri-Tiofilo <yusilva12345@gmail.com>
2020-02-27 21:33:08 -0300

    Final App Counter People

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: "Contator de Pessoas",
-      home: Home()));
+  runApp(MaterialApp(title: "Contator de Pessoas", home: Home()));
 }
 
 class Home extends StatefulWidget {
@@ -12,64 +10,80 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _people = 0;
+  String _infoText = "";
+
+  void _changePeople(int delta) {
+    setState(() {
+      _people += delta;
+
+      if(_people < 0) {
+        _infoText = "Mundo invertido";
+      } else if( _people <= 10) {
+        _infoText = "Pode entrar";
+      } else {
+        _infoText = "Lotado";
+      }
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return
-        Stack(
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.asset(
-              "images/restaurant.jpg",
-              fit: BoxFit.cover,
-              height: 1000.0,
+            Text(
+              "Pessoas: $_people",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
             ),
-            Column(
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text(
-                  "Pessoas: 0",
-                  style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: FlatButton(
-                        child: Text(
-                          "+1",
-                          style: TextStyle(fontSize: 40.0, color: Colors.white),
-                        ),
-                        onPressed: () {},
-                      ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
                     ),
-                    Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: FlatButton(
-                        child: Text(
-                          "-1",
-                          style: TextStyle(fontSize: 40.0, color: Colors.white),
-                        ),
-                        onPressed: () {},
-                      ),
-                    ),
-                  ],
+                    onPressed: () {
+                      _changePeople(1);
+                    },
+                  ),
                 ),
-                Text(
-                  "Pode entrar",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontStyle: FontStyle.italic,
-                      fontSize: 30.0),
-                )
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
+                  ),
+                ),
               ],
+            ),
+            Text(
+              _infoText,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
             )
           ],
-
+        )
+      ],
     );
   }
 }
-
-
-
-
